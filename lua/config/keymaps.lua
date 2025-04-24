18f904a96b05023bc9bf7849d8ba7645fd8444c9
habeeb65 <117671296+habeeb65@users.noreply.github.com>
2025-04-25 00:50:50 +0530

    pushing my lazyvim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+--
+-- Keymaps are automatically loaded on the VeryLazy even
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua-- Add any additional keymaps her
+
+-- jk to escape insert mode
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+
+-- Ctrl+S to escape insert mode
+vim.keymap.set("i", "<C-s>", "<Esc>", { desc = "Exit insert mode with Ctrl+S" })
+
+-- Move to beginning of line with H
+vim.keymap.set("n", "H", "^", { desc = "Go to start of line" })
+
+-- Move to end of line with L
+vim.keymap.set("n", "L", "$", { desc = "Go to end of line" })
+
+vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+
+vim.keymap.set({ "n", "i", "v" }, "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+
+vim.opt.clipboard = "unnamedplus"
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -27,6 +27,13 @@ return {
     opts = { use_diagnostic_signs = true },
   },
 
+  -- change trouble config
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+
   -- disable trouble
   { "folke/trouble.nvim", enabled = false },
 
